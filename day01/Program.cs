dd9a9d4a547e4f47187d92331e23a3ef4aa5b7ac
Nikola Beneš <nikola.benes@gmail.com>
2024-12-01 19:56:23 +0100

    Day 1: Use the more general Math.Abs function

--- day01/Program.cs
+++ day01/Program.cs
@@ -20,7 +20,7 @@ var lists = (
 ).ToArray();
 
 // Part 1
-Console.WriteLine(lists[0].Zip(lists[1], (a, b) => int.Abs(a - b)).Sum());
+Console.WriteLine(lists[0].Zip(lists[1], (a, b) => Math.Abs(a - b)).Sum());
 
 // Part 2
 var freq = (
