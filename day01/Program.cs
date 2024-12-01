e6d42b5fe931de8cb9e23eb118accceb0617b648
Nikola Beneš <nikola.benes@gmail.com>
2024-12-01 17:50:01 +0100

    Day 1: Add solution

---
+++ day01/Program.cs
@@ -0,0 +1,36 @@
+﻿IEnumerable<string> ConsoleLines() {
+	string? line;
+	while ((line = Console.ReadLine()) != null) {
+		yield return line;
+	}
+}
+
+var lists = (
+	from n_i in (
+		from line in ConsoleLines()
+		select line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+			.Select(Int32.Parse).ToArray()
+	).SelectMany(p => p.Select((n, i) => (n, i)))
+	group n_i by n_i.Item2 into g
+	orderby g.Key
+	select (
+		from n_i in g
+		orderby n_i.Item1
+		select n_i.Item1
+	).ToArray()
+).ToArray();
+
+// Part 1
+Console.WriteLine(lists[0].Zip(lists[1], (a, b) => Int32.Abs(a - b)).Sum());
+
+// Part 2
+var freq = (
+	from n in lists[1]
+	group n by n into g
+	select (g.Key, g.Count())
+).ToDictionary();
+
+Console.WriteLine((
+	from n in lists[0]
+	select n * freq.GetValueOrDefault(n, 0)
+).Sum());
