f5de5f98ecd1c89ac4226f5be86b2dc01fecd57f
Nikola Beneš <nikola.benes@gmail.com>
2024-12-01 19:51:47 +0100

    Day 1: Use int instead of Int32

--- day01/Program.cs
+++ day01/Program.cs
@@ -8,7 +8,7 @@
 var lists = (
 	from n_i in ConsoleLines().SelectMany(
 		line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
-			.Select((n, i) => (Int32.Parse(n), i))
+			.Select((n, i) => (int.Parse(n), i))
 	)
 	group n_i by n_i.Item2 into g
 	orderby g.Key
@@ -20,7 +20,7 @@ var lists = (
 ).ToArray();
 
 // Part 1
-Console.WriteLine(lists[0].Zip(lists[1], (a, b) => Int32.Abs(a - b)).Sum());
+Console.WriteLine(lists[0].Zip(lists[1], (a, b) => int.Abs(a - b)).Sum());
 
 // Part 2
 var freq = (
