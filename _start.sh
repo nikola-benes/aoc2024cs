9ebcc7ad25eda5c9c574a89c80b4c29f729d360a
Nikola Beneš <nikola.benes@gmail.com>
2024-12-08 19:07:25 +0100

    Add start-of-the-day script

---
+++ _start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+Y=2024
+C=  # Insert session cookie here
+A="nikola.benes@gmail.com via curl"
+
+if [ "$C" = "" ]; then
+	echo "Missing session cookie."
+	exit
+fi
+
+DAY="$1"
+DIR="day"$(printf '%02d' "$1")
+
+if [ "$DAY" = "" ]; then
+	echo "Usage: $0 <day_number>"
+	exit
+fi
+
+dotnet new console -o "$DIR"
+
+curl "https://adventofcode.com/$Y/day/$DAY/input" \
+	--cookie "session=$C" --user-agent "$A" > "$DIR/input"
