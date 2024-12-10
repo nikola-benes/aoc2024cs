9781dc65d5b098c768a86cdade6c9854000bec61
Nikola Beneš <nikola.benes@gmail.com>
2024-12-10 09:08:53 +0100

    Add some utilities for 2D grids

--- aoc/Aoc.cs
+++ aoc/Aoc.cs
@@ -9,6 +9,44 @@ public static class Aoc {
 	}
 }
 
+public record struct Vec2(int x, int y) {
+	public static implicit operator Vec2((int x, int y) p)
+		=> new Vec2(p.x, p.y);
+
+	public static Vec2 operator +(Vec2 a, Vec2 b)
+		=> new Vec2(a.x + b.x, a.y + b.y);
+
+	public static Vec2 operator -(Vec2 a, Vec2 b)
+		=> new Vec2(a.x - b.x, a.y - b.y);
+
+	public static Vec2 operator *(int k, Vec2 v)
+		=> new Vec2(k * v.x, k * v.y);
+
+	public static Vec2 operator *(Vec2 v, int k)
+		=> new Vec2(v.x * k, v.y * k);
+}
+
+public struct Grid {
+	string[] grid;
+
+	public Grid(IEnumerable<string> rows) { grid = rows.ToArray(); }
+
+	public int Height { get => grid.Length; }
+	public int Width { get => grid[0].Length; }
+
+	public IEnumerable<(char c, Vec2 pos)> Tiles { get =>
+		grid.SelectMany((row, y)
+			=> row.Select((c, x) => (c, new Vec2(x, y)))); }
+
+	public bool InBounds(Vec2 p)
+		=> 0 <= p.y && p.y < Height
+		&& 0 <= p.x && p.x < Width;
+
+	public char this[Vec2 p] => grid[p.y][p.x];
+
+	public char GetOr(Vec2 p, char d) => InBounds(p) ? this[p] : d;
+}
+
 public static class LinkedListExtensions {
 	public static
 	LinkedListNode<T>?
@@ -18,4 +56,9 @@ public static class LinkedListExtensions {
 			ptr = node.Next;
 		return ptr;
 	}
+}
+
+public static class EnumerableExtensions {
+	public static
+	Grid ToGrid(this IEnumerable<string> rows) => new Grid(rows);
 }
