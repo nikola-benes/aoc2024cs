be8e09954de016c3650e9a86ab0e90707917677a
Nikola Beneš <nikola.benes@gmail.com>
2024-12-18 15:54:59 +0100

    Day 18: Recompute only when needed

--- day18/Program.cs
+++ day18/Program.cs
@@ -5,6 +5,7 @@ var bytes = Aoc.ConsoleLines().Select(line => {
 	return new Vec2(nums[0], nums[1]);
 }).ToArray();
 
+Vec2 start = (0, 0);
 Vec2 goal = bytes.Length < 100 ? (6, 6) : (70, 70);
 int fallen = bytes.Length < 100 ? 12 : 1024;
 var (w, h) = goal + (1, 1);
@@ -13,20 +14,29 @@ var blocked = bytes.Take(fallen).ToHashSet();
 var IsEmpty = (Vec2 p)
 	=> 0 <= p.x && p.x < w && 0 <= p.y && p.y < h && !blocked.Contains(p);
 
+HashSet<Vec2> onPath = new();
+
 int? ShortestPath() {
 	Queue<Vec2> queue = new();
-	Dictionary<Vec2, int> dist = new();
-	queue.Enqueue((0, 0));
-	dist[(0, 0)] = 0;
+	Dictionary<Vec2, (int, Vec2)> distPrev = new();
+	queue.Enqueue(start);
+	distPrev[start] = (0, start);
+	onPath.Clear();
 
 	while (queue.TryDequeue(out var pos)) {
-		var d = dist[pos];
+		var (d, _) = distPrev[pos];
 		foreach (var dir in Grid.dirs4) {
 			var npos = pos + dir;
-			if (npos == goal)
+			if (npos == goal) {
+				while (pos != start) {
+					onPath.Add(pos);
+					(_, pos) = distPrev[pos];
+				}
 				return d + 1;
+			}
 
-			if (IsEmpty(npos) && dist.TryAdd(npos, d + 1))
+			if (IsEmpty(npos)
+					&& distPrev.TryAdd(npos, (d + 1, pos)))
 				queue.Enqueue(npos);
 		}
 	}
@@ -38,7 +48,7 @@ Console.WriteLine(ShortestPath());
 
 foreach (var next in bytes.Skip(fallen)) {
 	blocked.Add(next);
-	if (ShortestPath() is null) {
+	if (onPath.Contains(next) && ShortestPath() is null) {
 		Console.WriteLine("{0},{1}", next.x, next.y);
 		break;
 	}
