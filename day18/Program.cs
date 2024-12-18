fa0742317ce5f658b1b0fd8fda127ede3cde3c8e
Nikola Beneš <nikola.benes@gmail.com>
2024-12-18 17:23:25 +0100

    Day 18: Solve part 2 with a single graph search
    … because using binary search is too mainstream. :-)

--- day18/Program.cs
+++ day18/Program.cs
@@ -14,30 +14,47 @@ var blocked = bytes.Take(fallen).ToHashSet();
 var IsEmpty = (Vec2 p)
 	=> 0 <= p.x && p.x < w && 0 <= p.y && p.y < h && !blocked.Contains(p);
 
-HashSet<Vec2> onPath = new();
-
 int? ShortestPath() {
 	Queue<Vec2> queue = new();
-	Dictionary<Vec2, (int, Vec2)> distPrev = new();
+	Dictionary<Vec2, int> dist = new();
 	queue.Enqueue(start);
-	distPrev[start] = (0, start);
-	onPath.Clear();
+	dist[start] = 0;
 
 	while (queue.TryDequeue(out var pos)) {
-		var (d, _) = distPrev[pos];
+		var d = dist[pos];
 		foreach (var dir in Grid.dirs4) {
 			var npos = pos + dir;
-			if (npos == goal) {
-				while (pos != start) {
-					onPath.Add(pos);
-					(_, pos) = distPrev[pos];
-				}
+			if (npos == goal)
 				return d + 1;
-			}
 
-			if (IsEmpty(npos)
-					&& distPrev.TryAdd(npos, (d + 1, pos)))
+			if (IsEmpty(npos) && dist.TryAdd(npos, d + 1))
 				queue.Enqueue(npos);
+		}
+	}
+
+	return null;
+}
+
+int? FirstBlocked() {
+	PriorityQueue<Vec2, int> pq = new();
+	HashSet<Vec2> seen = new();
+	var bTime = bytes.Select((p, i) => (p, i)).ToDictionary();
+	var max = bytes.Length;
+	pq.Enqueue(start, -max);
+
+	// this is a graph search with a priority queue that is *not* Dijkstra
+	while (pq.TryDequeue(out var pos, out var first)) {
+		first = -first;
+
+		foreach (var dir in Grid.dirs4) {
+			var npos = pos + dir;
+			if (npos == goal)
+				return first;
+
+			if (IsEmpty(npos) && seen.Add(npos)) {
+				var block = bTime.GetValueOrDefault(npos, max);
+				pq.Enqueue(npos, -Math.Min(first, block));
+			}
 		}
 	}
 
@@ -46,10 +63,5 @@ int? ShortestPath() {
 
 Console.WriteLine(ShortestPath());
 
-foreach (var next in bytes.Skip(fallen)) {
-	blocked.Add(next);
-	if (onPath.Contains(next) && ShortestPath() is null) {
-		Console.WriteLine("{0},{1}", next.x, next.y);
-		break;
-	}
-}
+var b = bytes[FirstBlocked()!.Value];
+Console.WriteLine("{0},{1}", b.x, b.y);
